9deef6d17e10ff4b31b7b625383a4fddea5c1162
Minh Tuan <minhtuan9922@gmail.com>
2020-04-17 00:28:49 +0700

    cap nhat

--- catalog/controller/common/home.php
+++ catalog/controller/common/home.php
@@ -9,6 +9,84 @@ class ControllerCommonHome extends Controller {
 			$this->document->addLink($this->config->get('config_url'), 'canonical');
 		}
 
+		$this->load->model('catalog/category');
+		$this->load->model('catalog/product');
+		$this->load->model('tool/image');
+
+		$data['categories'] = array();
+
+		$categories = $this->model_catalog_category->getCategories(0);
+
+		$limit = 4;
+
+		foreach ($categories as $category) {
+			if ($category['top']) {
+				$products = array();
+                $filter_data = array(
+                    'filter_category_id' => $category['category_id'],
+                    'start'              => (1 - 1) * $limit,
+                    'limit'              => $limit
+                );
+
+                $results = $this->model_catalog_product->getProducts($filter_data);
+
+                foreach ($results as $result) {
+                    if ($result['image']) {
+                        $image = $this->model_tool_image->resize($result['image'], $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
+                    } else {
+                        $image = $this->model_tool_image->resize('placeholder.png', $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_width'), $this->config->get('theme_' . $this->config->get('config_theme') . '_image_product_height'));
+                    }
+
+                    if ($this->customer->isLogged() || !$this->config->get('config_customer_price')) {
+                        $price = $this->currency->format($this->tax->calculate($result['price'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+                    } else {
+                        $price = false;
+                    }
+
+                    if ((float)$result['special']) {
+                        $special = $this->currency->format($this->tax->calculate($result['special'], $result['tax_class_id'], $this->config->get('config_tax')), $this->session->data['currency']);
+                    } else {
+                        $special = false;
+                    }
+
+                    if ($this->config->get('config_tax')) {
+                        $tax = $this->currency->format((float)$result['special'] ? $result['special'] : $result['price'], $this->session->data['currency']);
+                    } else {
+                        $tax = false;
+                    }
+
+                    if ($this->config->get('config_review_status')) {
+                        $rating = (int)$result['rating'];
+                    } else {
+                        $rating = false;
+                    }
+
+                    $products[] = array(
+                        'product_id'  => $result['product_id'],
+                        'thumb'       => $image,
+                        'name'        => $result['name'],
+                        'description' => utf8_substr(trim(strip_tags(html_entity_decode($result['description'], ENT_QUOTES, 'UTF-8'))), 0, $this->config->get('theme_' . $this->config->get('config_theme') . '_product_description_length')) . '..',
+                        'price'       => $price,
+                        'special'     => $special,
+                        'tax'         => $tax,
+                        'minimum'     => $result['minimum'] > 0 ? $result['minimum'] : 1,
+                        'rating'      => $result['rating'],
+                        'href'        => $this->url->link('product/product', 'path=' . $category['category_id'] . '&product_id=' . $result['product_id'] )
+                    );
+                }
+
+				// Level 1
+				$data['categories'][] = array(
+					'name'     => $category['name'],
+					//'children' => $children_data,
+					//'column'   => $category['column'] ? $category['column'] : 1,
+					'href'     => $this->url->link('product/category', 'path=' . $category['category_id']),
+                    'image'    => $this->model_tool_image->resize($category['image'], 80, 80),
+                    'products' => $products,
+				);
+			}
+		}
+
 		$data['column_left'] = $this->load->controller('common/column_left');
 		$data['column_right'] = $this->load->controller('common/column_right');
 //		$data['column_right'] = $this->load->controller('common/column_right1');
